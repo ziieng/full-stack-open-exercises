6445c06872e3a32e243746a7c5c502f61e79b7c5
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-01 18:24:04 -0800

    exercise 1.4 - refactoring to use an array of objects

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,17 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = (props) => {
     return <h1>{props.course}</h1>
@@ -23,21 +25,25 @@ const App = () => {
 
   const Content = (props) => {
     return <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />    
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />    
+      <Part part={props.parts[2]} />
     </>
   }
 
   const Total = (props) => {
-    return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    let sum = 0
+    props.parts.forEach(part => {
+      sum = sum + part.exercises
+    })
+    return <p>Number of exercises {sum}</p>
   }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
