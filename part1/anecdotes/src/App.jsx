519775ea5858432fce8a2527a3fdbf31da428afa
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-02 19:38:50 -0800

    exercise 1.12 - anecdotes step 1

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,51 +1,27 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const Header = (props) => {
-    return <h1>{props.course}</h1>
-  }
+  const randomize = () => Math.floor(Math.random() * anecdotes.length)
+   
+  const [selected, setSelected] = useState(0)
 
-  const Part = ({ part }) => {
-    return <p>{part.name} {part.exercises}</p>
-  }
-
-  const Content = (props) => {
-    return <>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />    
-      <Part part={props.parts[2]} />
-    </>
-  }
-
-  const Total = (props) => {
-    let sum = 0
-    props.parts.forEach(part => {
-      sum = sum + part.exercises
-    })
-    return <p>Number of exercises {sum}</p>
-  }
+  const handleRandomClick = () => { setSelected(randomize()) }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleRandomClick}>next anecdote</button>
     </div>
   )
 }
