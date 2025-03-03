c3939f0b4f9b8925b5c720e3913c3d8853963068
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-02 20:11:48 -0800

    restore previously mislabeled and overwritten courseinfo folder

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const Header = (props) => {
+    return <h1>{props.course}</h1>
+  }
+  
+  const Part = ({ part }) => {
+    return <p>{part.name} {part.exercises}</p>
+  }
+
+  const Content = (props) => {
+    return <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />    
+      <Part part={props.parts[2]} />
+    </>
+  }
+
+  const Total = (props) => {
+    let sum = 0
+    props.parts.forEach(part => {
+      sum = sum + part.exercises
+    })
+    return <p>Number of exercises {sum}</p>
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+  
+export default App;
