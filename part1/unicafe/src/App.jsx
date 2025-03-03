2873c04ec7f8983a35fb281ca82dd65e00fc32eb
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-02 19:32:09 -0800

    exercise 1.11 - unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,20 +4,27 @@ const Title = ({text}) => <h1>{text}</h1>
 
 const Button = ({onClick, label}) => <button onClick={onClick}>{label}</button>
 
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
   
   if (total > 0) {
-    return (<>
-    <StatisticLine text="good" value={good} />
-    <StatisticLine text="neutral" value={neutral} />
-    <StatisticLine text="bad" value={bad} />
-    <StatisticLine text="all" value={total} />
-    <StatisticLine text="average" value={(good - bad) / total} />
-    <StatisticLine text="positive" value={`${good / total * 100}%`} />
-    </>)
+    return (<table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={(good - bad) / total} />
+        <StatisticLine text="positive" value={`${good / total * 100}%`} />
+      </tbody>
+    </table>)
   }
 
   return <p>No feedback given</p>
