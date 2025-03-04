470065c333884b992f2171919e367053559cc61b
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-03 21:22:59 -0800

    exercise 2.5 - course information step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,4 @@
-const Course = ({course}) => {
-  const Header = ({name}) => {
-    return <h1>{name}</h1>
-  }
-  
-  const Part = ({ part }) => {
-    return <p>{part.name} {part.exercises}</p>
-  }
-
-  const Content = ({ parts }) => {
-    return parts.map((part) => {
-      return <Part part={part} key={part.id} />
-    })
-  }
-
-  const Total = ({ parts }) => {
-    const sum = parts.reduce((acc, part) => acc + part.exercises, 0)
-    return <strong>total of {sum} exercises</strong>
-  }
-
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./Course"
 
 const App = () => {
   const courses = [
