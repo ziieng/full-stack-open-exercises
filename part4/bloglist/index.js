301460afeecb7b689e1e26653e2d2b5c11c1b1eb
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-29 17:11:23 -0700

    refactor model out of index.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,15 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
