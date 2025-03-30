3b59e58ad164e2ffb5db83f7e811b8783ed71e4f
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-29 21:37:49 -0700

    exercise 4.5 - favorite blog helper

---
+++ part4/bloglist/tests/list_helper.test.js
@@ -0,0 +1,85 @@
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
+
+const listWithOneBlog = [
+  {
+    id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+  },
+];
+
+const listWithManyBlogs = [
+  {
+    title: "Some guy's blog",
+    author: "Some Guy",
+    url: "http://example.com",
+    likes: 5,
+    id: "67e88883fc558a847cb1a349",
+  },
+  {
+    title: "Some lady's blog",
+    author: "Some Lady",
+    url: "http://example.com",
+    likes: 10,
+    id: "67e88894fc558a847cb1a34b",
+  },
+  {
+    title: "Another lady's blog",
+    author: "Some Lady",
+    url: "http://example.com",
+    likes: 15,
+    id: "67e888a3fc558a847cb1a34d",
+  },
+];
+
+describe("total likes helper", () => {
+  test("when list is empty, returns 0", () => {
+    const blogs = [];
+
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has one blog, returns the likes of that blog", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("when list has many blogs, returns the total of all likes", () => {
+    const result = listHelper.totalLikes(listWithManyBlogs);
+    assert.strictEqual(result, 30);
+  });
+});
+
+describe("favorite blog helper", () => {
+  test("when list is empty, returns null", () => {
+    const blogs = [];
+
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, null);
+  });
+
+  test("when list has one blog, returns that blog with simplified fields", () => {
+    const expectedResult = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    }
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    assert.deepStrictEqual(result, expectedResult);
+  });
+
+  test("when list has many blogs, returns the one with the highest likes", () => {
+    const expectedResult = {
+      title: "Another lady's blog",
+      author: "Some Lady",
+      likes: 15,
+    }
+    const result = listHelper.favoriteBlog(listWithManyBlogs);
+    assert.deepStrictEqual(result,expectedResult);
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  const favorite = blogs.reduce((prev, current) => {
+    return (prev && prev.likes > current.likes) ? prev : current
+  });
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
